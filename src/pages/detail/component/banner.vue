72aa844eb3f8e911969310b88ce0545641cfc811
rose <rose_joh@163.com>
2019-10-30 11:08:05 +0800

    banner样式完成

--- src/pages/detail/component/banner.vue
+++ src/pages/detail/component/banner.vue
@@ -6,8 +6,8 @@
       alt="欢乐谷"
     >
     <div class="banner-info">
-      <div class="banner-number">51</div>
       <div class="banner-title">深圳欢乐谷(AAAAA景区)</div>
+       <div class="banner-number"><span class="iconfont banner-icon">&#xe691;</span>51</div>
     </div>
   </div>
 </template>
@@ -18,6 +18,7 @@ export default {
 </script>
 <style lang="stylus" scoped>
   .banner
+    position relative
     overflow hidden
     height 0
     padding-bottom 55%
@@ -30,12 +31,22 @@ export default {
       right 0
       bottom 0
       line-height .6rem
+      color #ffffff
+      padding 0 .2rem
+      background-image linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
       .banner-title
         flex 1
+        font-size .32rem
+        padding 0 .2rem
       .banner-number
-        padding 0 .4re
-        height .4rem
-        line-height .4rem
+        margin-top .2rem
+        padding 0 .2rem
+        height .32rem
+        line-height .32rem
         border-radius .2rem
-        // background:#rgba(0,0,0,.8)
+        background rgba(0,0,0,.8)
+        font-size .24rem
+        .banner-icon
+          font-size .1rem
+          margin 0 .1rem 0 0
 </style>
