d4fb5847f0c090228c1c8eb252e5fc8552596a91
e <rose_joh@163.com>
2019-10-30 21:58:46 +0800

    banner样式更新

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="container">
     <div class="wrapper">
-      123
+      <swiper >
+        <swiper-slide>
+          <img 
+            class="detail-image" 
+            src="http://img1.qunarzz.com/sight/p0/201403/07/a3231208ca4095bef77068c202d95bd2.jpg_r_800x800_44f2e684.jpg" 
+            alt="去哪儿网"
+          >
+        </swiper-slide>
+        <swiper-slide>
+          <img 
+            class="detail-image" 
+            src="http://img1.qunarzz.com/sight/p0/1908/6c/6c4f551349dd8896a3.img.jpg_r_800x800_9593721e.jpg" 
+            alt="去哪儿网"
+          >
+        </swiper-slide>
+        <!-- <div class="swiper-pagination"  slot="pagination"></div>
+        <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+      </swiper>
     </div>
   </div>
    
@@ -24,5 +41,10 @@ export default {
     bottom 0
     background #000
     .wrapper
-      background #ffffff
+      overflow hidden
+      width 100%
+      height 0
+      padding-bottom 100%
+      .detail-image
+        width 100%
 </style>
